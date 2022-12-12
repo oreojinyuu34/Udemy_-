2a9c87e16c9f701e19e838833a15b8c32a16afcd
atsushi <77239082+oreojinyuu34@users.noreply.github.com>
2022-12-12 23:59:23 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -58,3 +58,30 @@
 // //テンプレート文字列を用いた方法(バッククォートの中に書く)
 // const message2 = `私の名前は${name}です。年齢は${age}歳です。`
 // console.log(message2);
+
+
+/**
+ * アロー関数
+ */
+
+//従来の関数
+// function func1(str) {
+//   return str;
+// }
+
+const func1 =function(str) {
+  return str;
+}
+console.log(func1("func1です"));
+
+const func2 = (str) => {
+  return str;
+}
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+
+
+console.log(func3(10,20));
